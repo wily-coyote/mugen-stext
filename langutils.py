fdcfac44b8ba2f46a10065be57b6e473251b1ef0
wily-coyote <24258996+wily-coyote@users.noreply.github.com>
2024-11-12 11:38:55 -0500

    use the new nvim format and don't hover on empty lines

--- langutils.py
+++ langutils.py
@@ -6,41 +6,49 @@ from .utils.data import data as data
 
 _args_re = re.compile(r"[\(,\)\*\s]+")
 
+_fmt_money_re = re.compile(r"\$([A-Za-z_]+[0-9]?)")
 _mugen_doc_format = "<html><body><b>{}</b><p>{}</p><a href=\"{}\">Elecbyte documentation</a><br><a href=\"{}\">MFG documentation (DDG)</a></body></html>"
 _ikemen_doc_format = "<html><body><b>{}</b><p>{}</p><a href=\"{}\">Github documentation</a></body></html>"
 
 def sctrl_doc(sctrl):
-	if sctrl in data["mugen-sctrl"]:
-		return _mugen_doc_format.format(
-				sctrl,
-				data["mugen-sctrl"][sctrl]["doc"],
-				"http://www.elecbyte.com/mugendocs/sctrls.html#{}".format(urllib.parse.quote_plus(sctrl.lower())),
-				"https://duckduckgo.com/?q=!ducky+site:mugenguild.com+{}+AND+%22sctrl%22".format(urllib.parse.quote_plus("\""+sctrl+"\""))
-			)
-	elif sctrl in data["ikemen-sctrl"]:
+	found = None
+	for k in data["sctrl"]:
+		if k.lower() == sctrl.lower():
+			found = data["sctrl"][k]
+			break
+	if found["ikgo"]:
 		return _ikemen_doc_format.format(
-				sctrl,
-				data["ikemen-sctrl"][sctrl]["doc"],
-				"https://github.com/ikemen-engine/Ikemen-GO/wiki/State-controllers#{}".format(urllib.parse.quote_plus(sctrl.lower()))
-			)
+			sctrl,
+			found["doc"],
+			"https://github.com/ikemen-engine/Ikemen-GO/wiki/State-controllers#{}".format(urllib.parse.quote_plus(sctrl.lower()))
+		)
+	else:
+		return _mugen_doc_format.format(
+			sctrl,
+			found["doc"],
+			"http://www.elecbyte.com/mugendocs/sctrls.html#{}".format(urllib.parse.quote_plus(sctrl.lower())),
+			"https://duckduckgo.com/?q=!ducky+site:mugenguild.com+{}+AND+%22sctrl%22".format(urllib.parse.quote_plus("\""+sctrl+"\""))
+		)
 
 def trigger_doc(trigger):
-	if trigger in data["mugen-trigger"]:
-		return _mugen_doc_format.format(
-				trigger,
-				data["mugen-trigger"][trigger]["doc"],
-				"http://www.elecbyte.com/mugendocs/trigger.html#{}".format(urllib.parse.quote_plus(_args_re.sub("-", trigger.lower()).strip("-"))),
-				"https://duckduckgo.com/?q=!ducky+site:mugenguild.com+{}+AND+%22trigger%22".format(urllib.parse.quote_plus("\""+trigger+"\""))
-			)
-	elif trigger in data["ikemen-trigger"]:
+	found = None
+	for k in data["trigger"]:
+		if k.lower() == trigger.lower():
+			found = data["trigger"][k]
+			break
+	if found["ikgo"]:
 		return _ikemen_doc_format.format(
-				trigger,
-				data["ikemen-trigger"][trigger]["doc"],
-				"https://github.com/ikemen-engine/Ikemen-GO/wiki/Triggers#{}".format(urllib.parse.quote_plus(trigger.lower()))
-			)
-
-_sctrl_index = list(data["mugen-sctrl"].keys())+list(data["ikemen-sctrl"].keys())
-_trigger_index = list(data["mugen-trigger"].keys())+list(data["ikemen-trigger"].keys())
+			trigger,
+			found["doc"],
+			"https://github.com/ikemen-engine/Ikemen-GO/wiki/Triggers#{}".format(urllib.parse.quote_plus(trigger.lower()))
+		)
+	else:
+		return _mugen_doc_format.format(
+			trigger,
+			found["doc"],
+			"http://www.elecbyte.com/mugendocs/trigger.html#{}".format(urllib.parse.quote_plus(_args_re.sub("-", trigger.lower()).strip("-"))),
+			"https://duckduckgo.com/?q=!ducky+site:mugenguild.com+{}+AND+%22trigger%22".format(urllib.parse.quote_plus("\""+trigger+"\""))
+		)
 
 class CNSCompletions(sublime_plugin.EventListener):
 	def on_query_completions(self, view, prefix, locations):
@@ -52,47 +60,87 @@ class CNSCompletions(sublime_plugin.EventListener):
 		prefix = prefix.lower()
 		out = []
 
+
 		def do_Trigger():
-			for key in _trigger_index:
+			for key in data["trigger"]:
 				if key.lower().startswith(prefix):
-					completed = (data["mugen-trigger"].get(key) or data["ikemen-trigger"].get(key)) or None
+					completed = data["trigger"].get(key) or None
 					if completed is None: continue
+					idx = 1
+					def lovepython(x):
+						nonlocal idx
+						ret = "${{{}:{}}}".format(idx, x.group(1))
+						idx = idx + 1
+						return ret
+					fmt = completed["fmt"]
+					fmt = _fmt_money_re.sub(lambda x: lovepython(x), fmt)
+					print(fmt)
 					item = sublime.CompletionItem(
 						key,
 						"trigger",
-						completed["fmt"],
-						0,
+						fmt,
+						1,
 						(7, "t", "trigger"),
 						"<a href='subl:show_trigger_documentation {{\"arg\": \"{}\"}}'>Show documentation</a>".format(key)
 					)
 					out.append(item)
 
-
-		if line.lower().strip().startswith("type") or is_zss:
-			for key in _sctrl_index:
-				if key.lower().startswith(prefix):
-					completed = (data["mugen-sctrl"].get(key) or data["ikemen-sctrl"].get(key)) or None
-					if completed is None: continue
-					content = ""
-					if is_zss:
-						content = key+"{{{}}}".format(";".join(["{}: ".format(x) for x in completed["req"]]))
-					else:
-						content = key+"\n"+"\n".join(["{}=".format(x) for x in ["trigger1"] + completed["req"]])+"\n"+"\n".join(["; {}=".format(x) for x in completed["opt"]])
-					item = sublime.CompletionItem(
-						key,
-						"sctrl",
-						content,
-						0,	
-						(3, "s", "sctrl"),
-						"<a href='subl:show_sctrl_documentation {{\"arg\": \"{}\"}}'>Show documentation</a>".format(key)
-					)
-					out.append(item)
-			if is_zss:
-				do_Trigger()
-		else:
-			if view.match_selector(locations[0], "meta.mapping.key.cns") or view.match_selector(locations[0], "punctuation.separator.mapping.key-value.cns"):
-				return
-			do_Trigger()
+		for key in data["sctrl"]:
+			if key.lower().startswith(prefix):
+				completed = data["sctrl"].get(key) or None
+				if completed is None: continue
+				content = ""
+				if is_zss:
+					content = key+"{\n"
+					idx = 1
+					if completed.get("req"):
+						for item in completed["req"]:
+							content += "\t{}: ".format(item["name"])
+							values = item["value"].copy()
+							for k in range(len(values)):
+								values[k] = "${{{}:{}}}".format(idx, values[k])
+								idx+=1
+							content += ", ".join(values) + ";\n"
+					if completed.get("opt"):
+						for item in completed["opt"]:
+							content += "\t# {}: ".format(item["name"])
+							values = item["value"].copy()
+							for k in range(len(values)):
+								values[k] = "${{{}:{}}}".format(idx, values[k])
+								idx+=1
+							content += ", ".join(values) + ";\n"
+					content += "}"
+				else:
+					idx = 1
+					content = "[State ${{{}:{}}}]\n".format(idx, key); idx += 1
+					content += "type={}\n".format(key)
+					content += "trigger1=${{{}:1}}\n".format(idx); idx += 1
+					if completed.get("req"):
+						for item in completed["req"]:
+							content += "{}=".format(item["name"])
+							values = item["value"].copy()
+							for k in range(len(values)):
+								values[k] = "${{{}:{}}}".format(idx, values[k])
+								idx+=1
+							content += ", ".join(values) + "\n"
+					if completed.get("opt"):
+						for item in completed["opt"]:
+							content += "; {}=".format(item["name"])
+							values = item["value"].copy()
+							for k in range(len(values)):
+								values[k] = "${{{}:{}}}".format(idx, values[k])
+								idx+=1
+							content += ", ".join(values) + "\n"
+				item = sublime.CompletionItem(
+					key,
+					"sctrl",
+					content,
+					1,
+					(3, "s", "sctrl"),
+					"<a href='subl:show_sctrl_documentation {{\"arg\": \"{}\"}}'>Show documentation</a>".format(key)
+				)
+				out.append(item)
+		do_Trigger()
 		return out
 
 class CNSTooltips(sublime_plugin.EventListener):
@@ -101,13 +149,14 @@ class CNSTooltips(sublime_plugin.EventListener):
 			return
 		is_zss = view.syntax().scope == "source.zss"
 		region = view.extract_scope(point)
-		text = view.substr(region)
+		word = view.substr(region)
 		line = view.substr(view.line(point))
+		if len(line) <= 0: return
 
 		def do_Trigger():
 			region = view.word(point)
 			word = view.substr(region)
-			found = sorted(list(filter(lambda x: x.strip().lower().startswith(word.lower()), _trigger_index)), key=len)
+			found = sorted(list(filter(lambda x: x.strip().lower().startswith(word.lower()), data["trigger"].keys())), key=len)
 			if len(found) > 0:
 				found = found[0]
 				view.show_popup(trigger_doc(found),
@@ -116,7 +165,7 @@ class CNSTooltips(sublime_plugin.EventListener):
 				max_width=420)
 
 		if line.strip().startswith("type") or is_zss:
-			found = list(filter(lambda x: text.strip().lower().startswith(x.lower()), _sctrl_index))
+			found = list(filter(lambda x: x.strip().lower().startswith(word.lower()), data["sctrl"].keys()))
 			if len(found) > 0:
 				found = found[0]
 				view.show_popup(sctrl_doc(found),
@@ -126,7 +175,6 @@ class CNSTooltips(sublime_plugin.EventListener):
 				return
 			if is_zss:
 				do_Trigger()
-	
 		else:
 			if view.match_selector(point, "meta.mapping.key.cns") or view.match_selector(point, "punctuation.separator.mapping.key-value.cns"):
 				return
--- utils/data.py
+++ utils/data.py
@@ -3,15 +3,56 @@ import json, sublime, os.path
 def getpath(bah):
 	return os.path.join(sublime.packages_path(), "MUGEN", "utils", bah)
 
-data = {}
+def safeget(list, index):
+	if index >= len(list):
+		return None
+	else:
+		return list[index]
 
-jsons = [
-	"mugen-sctrl",
-	"mugen-trigger",
-	"ikemen-sctrl",
-	"ikemen-trigger"
-]
+data = {
+	"sctrl": {},
+	"trigger": {}
+}
 
-for path in jsons:
-	with open(getpath(path+".json"), "r") as file:
-		data[path] = json.load(file)
+with open(getpath("data.tsv"), "r") as file:
+	obj = None
+	doc = None
+	while True:
+		line = file.readline()
+		if len(line) == 0:
+			break
+		line = line.rstrip().replace("\\n", "\n")
+		if len(line) > 0:
+			fields = line.split("\t")
+			if len(fields) >= 1:
+				field1 = safeget(fields, 0)
+				field2 = safeget(fields, 1)
+				field3 = safeget(fields, 2)
+				if field1 == "sctrl" or field1 == "trigger":
+					obj = {
+						"ikgo": field3 == "ikgo"
+					}
+					data[field1][field2] = obj
+				else:
+					if field1 == "opt" or field1 == "req":
+						item = {
+							"name": field2,
+							"value": []
+						}
+						for k in range(len(fields)):
+							if k >= 2:
+								item["value"].append(fields[k])
+						if obj.get(field1) is not None:
+							obj[field1].append(item)
+						else:
+							obj[field1] = [item]
+					elif field1 == "docbgn":
+						doc = ""
+						obj["doc"] = doc
+					elif field1 == "docend":
+						obj["doc"] = doc.rstrip().lstrip()
+						doc = None
+					elif doc is not None:
+						doc = doc + line + "\n"
+					elif (obj is not None) and (field1 is not None):
+						obj[field1] = field2
